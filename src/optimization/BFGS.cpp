b697437f9deb1243cc61aa09ea3d3eb405b4b9b3
ktns <ktns@1136aad2-a195-0410-b898-f5ea1d11b9d8>
2012-06-22 05:03:05 +0000

    Created new optimizer class BFGS. (not yet implemented) #28764

---
+++ src/optimization/BFGS.cpp
@@ -0,0 +1,71 @@
+//************************************************************************//
+// Copyright (C) 2011-2012 Mikiya Fujii                                   //
+// Copyright (C) 2012-2012 Katsuhiko Nishimra                             //
+//                                                                        //
+// This file is part of MolDS.                                            //
+//                                                                        //
+// MolDS is free software: you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or      //
+// (at your option) any later version.                                    //
+//                                                                        //
+// MolDS is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+// GNU General Public License for more details.                           //
+//                                                                        //
+// You should have received a copy of the GNU General Public License      //
+// along with MolDS.  If not, see <http://www.gnu.org/licenses/>.         //
+//************************************************************************//
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<sstream>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stdexcept>
+#include<boost/shared_ptr.hpp>
+#include<boost/format.hpp>
+#include"../base/PrintController.h"
+#include"../base/MolDSException.h"
+#include"../base/Uncopyable.h"
+#include"../base/Enums.h"
+#include"../base/EularAngle.h"
+#include"../base/Parameters.h"
+#include"../base/atoms/Atom.h"
+#include"../base/Molecule.h"
+#include"../base/ElectronicStructure.h"
+#include"Optimizer.h"
+#include"BFGS.h"
+using namespace std;
+using namespace MolDS_base;
+using namespace MolDS_base_atoms;
+
+namespace MolDS_optimization{
+BFGS::BFGS(){
+   this->SetMessages();
+   //this->OutputLog("BFGS created\n");
+}
+
+BFGS::~BFGS(){
+   //this->OutputLog("BFGS deleted\n");
+}
+
+void BFGS::SetMessages(){
+   Optimizer::SetMessages();
+   this->errorMessageNotEnebleTheoryType
+      = "Error in optimization::BFGS::CheckEnableTheoryType: Non available theory is set.\n";
+   this->errorMessageGeometyrOptimizationNotConverged
+      = "Error in optimization::BFGS::Optimize: Optimization did not met convergence criterion.\n";
+   this->messageStartBFGSStep = "\n==========  START: BFGS step ";
+}
+
+//void BFGS::SearchMinimum(boost::shared_ptr<ElectronicStructure> electronicStructure,
+//                         Molecule& molecule,
+//                         double* lineSearchedEnergy,
+//                         bool* obtainesOptimizedStructure) const
+//{
+//}
+
+}
