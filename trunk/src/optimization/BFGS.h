11f179649cbda1b851553f610e8b4051292164a7
ktns <ktns@1136aad2-a195-0410-b898-f5ea1d11b9d8>
2012-06-22 05:03:05 +0000

    Created new optimizer class BFGS. (not yet implemented) #28764

---
+++ trunk/src/optimization/BFGS.h
@@ -0,0 +1,39 @@
+//************************************************************************//
+// Copyright (C) 2011-2012 Mikiya Fujii                                   //
+// Copyright (C) 2012-2012 Katsuhiko Nishimra                             //
+//                                                                        //
+// This file is part of MolDS.                                            //
+//                                                                        //
+// MolDS is free software: you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or      //
+// (at your option) any later version.                                    //
+//                                                                        //
+// MolDS is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+// GNU General Public License for more details.                           //
+//                                                                        //
+// You should have received a copy of the GNU General Public License      //
+// along with MolDS.  If not, see <http://www.gnu.org/licenses/>.         //
+//************************************************************************//
+#ifndef INCLUDED_BFGS
+#define INCLUDED_BFGS
+namespace MolDS_optimization{
+
+class BFGS : public MolDS_optimization::Optimizer{
+public:
+   BFGS();
+   ~BFGS();
+protected:
+   void SetMessages();
+private:
+   std::string messageStartBFGSStep;
+   virtual void SearchMinimum(boost::shared_ptr<MolDS_base::ElectronicStructure> electronicStructure,
+                              MolDS_base::Molecule& molecule,
+                              double* lineSearchedEnergy,
+                              bool* obainesOptimizedStructure) const = 0;
+};
+
+}
+#endif
