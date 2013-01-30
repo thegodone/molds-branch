b06787bafe9ad29d840927ed508f110cdffce56e
mikiya_fujii <mikiya_fujii@1136aad2-a195-0410-b898-f5ea1d11b9d8>
2013-01-30 00:40:21 +0000

    revert to r1266 #30621

--- trunk/src/base/Molecule.h
+++ trunk/src/base/Molecule.h
@@ -46,7 +46,7 @@ public:
    void CalcXyzCOM();
    void CalcXyzCOC();
    void CalcBasics();
-   inline int GetTotalNumberAOs() const{return this->totalNumberAOs;}
+   int GetTotalNumberAOs() const;
    int GetTotalNumberValenceElectrons() const;
    double GetTotalCoreMass() const;
    void OutputXyzCOM() const;
