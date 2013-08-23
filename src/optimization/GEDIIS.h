4ed8bd24f90c01f1011fab5a78dc17d63b59fe9d
ktns <ktns@1136aad2-a195-0410-b898-f5ea1d11b9d8>
2013-08-23 07:06:21 +0000

    Declaration of GEDIIS optimization class. #31854

---
+++ src/optimization/GEDIIS.h
@@ -0,0 +1,38 @@
+//************************************************************************//
+// Copyright (C) 2011-2012 Mikiya Fujii                                   //
+// Copyright (C) 2012-2013 Katsuhiko Nishimra                             //
+//                                                                        //
+// This file is part of MolDS.                                            //
+//                                                                        //
+// MolDS is free software: you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or      //
+// (at your option) any later version.                                    //
+//                                                                        //
+// MolDS is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+// GNU General Public License for more details.                           //
+//                                                                        //
+// You should have received a copy of the GNU General Public License      //
+// along with MolDS.  If not, see <http://www.gnu.org/licenses/>.         //
+//************************************************************************//
+#ifndef INCLUDED_GEDIIS
+#define INCLUDED_GEDIIS
+namespace MolDS_optimization{
+
+class GEDIIS : public MolDS_optimization::BFGS{
+public:
+   GEDIIS();
+   ~GEDIIS();
+protected:
+   void SetMessages();
+private:
+   virtual void SearchMinimum(boost::shared_ptr<MolDS_base::ElectronicStructure> electronicStructure,
+                              MolDS_base::Molecule& molecule,
+                              double* lineSearchedEnergy,
+                              bool* obainesOptimizedStructure) const;
+};
+
+}
+#endif
