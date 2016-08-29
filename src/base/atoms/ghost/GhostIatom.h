8fe7c51c90de971f938a3743477a4645c2a0a081
thegodone <guillaume.godin@gmail.com>
2016-08-29 07:01:21 +0200

    adding GhostI & GhostBr

---
+++ src/base/atoms/ghost/GhostIatom.h
@@ -0,0 +1,32 @@
+//************************************************************************//
+// Copyright (C) 2011-2014 Mikiya Fujii                                   // 
+//                                                                        // 
+// This file is part of MolDS.                                            // 
+//                                                                        // 
+// MolDS is free software: you can redistribute it and/or modify          // 
+// it under the terms of the GNU General Public License as published by   // 
+// the Free Software Foundation, either version 3 of the License, or      // 
+// (at your option) any later version.                                    // 
+//                                                                        // 
+// MolDS is distributed in the hope that it will be useful,               // 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of         // 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          // 
+// GNU General Public License for more details.                           // 
+//                                                                        // 
+// You should have received a copy of the GNU General Public License      // 
+// along with MolDS.  If not, see <http://www.gnu.org/licenses/>.         // 
+//************************************************************************//
+#ifndef INCLUDED_GHOSTIATOM
+#define INCLUDED_GHOSTIATOM
+namespace MolDS_base_atoms_ghost{
+class GhostIatom : public MolDS_base_atoms::Iatom, public Ghost {
+public:
+   GhostIatom(int index);
+   ~GhostIatom();
+protected:
+   void virtual SetAtomicParameters();
+private:
+   GhostIatom();
+};
+}
+#endif
