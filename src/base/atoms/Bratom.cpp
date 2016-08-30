5dfc597f6f01b74e84f0fabaf054b886059f55cd
thegodone <guillaume.godin@gmail.com>
2016-08-30 08:50:22 +0200

    adding PM3 parameters for Br

--- src/base/atoms/Bratom.cpp
+++ src/base/atoms/Bratom.cpp
@@ -76,20 +76,35 @@ void Bratom::SetAtomicParameters(){
    this->imuAmuP = 11.080*Parameters::GetInstance()->GetEV2AU();
    this->imuAmuD = 0.0;
    // END ????
-   this->mndoHeatsFormAtom =  26.735 * Parameters::GetInstance()->GetKcalMolin2AU(); //Handbook of Bond Dissociation Energies in Organic Compounds
-   // from MOPAC 7 ok
-   this->mndoGss = 15.03643948 * Parameters::GetInstance()->GetEV2AU();   
-   this->mndoGpp = 11.27632539 * Parameters::GetInstance()->GetEV2AU();   
-   this->mndoGsp = 13.03468242 * Parameters::GetInstance()->GetEV2AU();   
-   this->mndoGpp2 = 9.85442552 * Parameters::GetInstance()->GetEV2AU();  
-   this->mndoHsp =  2.45586832 * Parameters::GetInstance()->GetEV2AU();   
-   // END OK MOPAC
    // what about this ? in ORCA it's look like
    this->effectiveNuclearChargeK = 8.7;
    this->effectiveNuclearChargeL = 5.20;
    this->effectiveNuclearChargeMsp = 0.0;
    this->effectiveNuclearChargeMd = 0.0;
    // END what about this ? in ORCA it's look like
+   // from MOPAC 7 + Extension of the PDDG/PM3 and PDDG/MNDO Semiempirical Molecular Orbital Methods to the Halogens
+   // J Comput Chem 25: 138 –150, 2004
+   this->mndoCoreintegralS = -99.986441 * Parameters::GetInstance()->GetEV2AU();
+   this->mndoCoreintegralP = -75.671308 * Parameters::GetInstance()->GetEV2AU();
+   this->mndoOrbitalExponentS = 3.854302;
+   this->mndoOrbitalExponentP = 2.199209;
+   this->mndoBondingParameterS = -8.917107 * Parameters::GetInstance()->GetEV2AU();
+   this->mndoBondingParameterP = -9.943740 * Parameters::GetInstance()->GetEV2AU();     
+   this->mndoAlpha = 2.445705 / Parameters::GetInstance()->GetAngstrom2AU();        
+   this->mndoElecEnergyAtom = -346.681250 * Parameters::GetInstance()->GetEV2AU();        
+   this->mndoHeatsFormAtom =  26.735 * Parameters::GetInstance()->GetKcalMolin2AU(); //Handbook of Bond Dissociation Energies in Organic Compounds
+   this->mndoGss = 15.03643948 * Parameters::GetInstance()->GetEV2AU();   
+   this->mndoGpp = 11.27632539 * Parameters::GetInstance()->GetEV2AU();   
+   this->mndoGsp = 13.03468242 * Parameters::GetInstance()->GetEV2AU();   
+   this->mndoGpp2 = 9.85442552 * Parameters::GetInstance()->GetEV2AU();  
+   this->mndoHsp =  2.45586832 * Parameters::GetInstance()->GetEV2AU();   
+   this->mndoDerivedParameterD[0] =   0;
+   this->mndoDerivedParameterD[1] =   0;
+   this->mndoDerivedParameterD[2] =   0;    
+   this->mndoDerivedParameterRho[0] = 0.5/1;  
+   this->mndoDerivedParameterRho[1] = 0.5/1;
+   this->mndoDerivedParameterRho[2] = 0.5/1;
+   // END OK MOPAC & J Comput Chem 25: 138 –150, 2004
    // OK came from MOPAC 7 & ORIGINAL PUBLICATION
    this->am1CoreintegralS = -104.656063 * Parameters::GetInstance()->GetEV2AU();         
    this->am1CoreintegralP = -74.930052 * Parameters::GetInstance()->GetEV2AU();         
@@ -150,5 +165,31 @@ void Bratom::SetAtomicParameters(){
    this->rm1ParameterM[1] = 2.01617695 * Parameters::GetInstance()->GetAngstrom2AU();
    this->rm1ParameterM[2] = 0.0 * Parameters::GetInstance()->GetAngstrom2AU();
    this->rm1ParameterM[3] = 0.0 * Parameters::GetInstance()->GetAngstrom2AU();
+   this->pm3CoreintegralS = -116.619310 * Parameters::GetInstance()->GetEV2AU();         
+   this->pm3CoreintegralP =  -74.227130 * Parameters::GetInstance()->GetEV2AU();         
+   this->pm3OrbitalExponentS = 5.348460;      
+   this->pm3OrbitalExponentP = 2.127590;      
+   this->pm3BondingParameterS = -31.171340 * Parameters::GetInstance()->GetEV2AU();     
+   this->pm3BondingParameterP = -6.814010 * Parameters::GetInstance()->GetEV2AU();     
+   this->pm3Alpha = 2.511840 / Parameters::GetInstance()->GetAngstrom2AU();        
+   this->pm3DerivedParameterD[0] = 0.0;    
+   this->pm3DerivedParameterD[1] = 0;    
+   this->pm3DerivedParameterD[2] = 0;    
+   this->pm3DerivedParameterRho[0] = 0.5/1;
+   this->pm3DerivedParameterRho[1] = 0.5/1;  
+   this->pm3DerivedParameterRho[2] = 0.5/1;  
+   this->pm3ParameterK[0] = 0.960460 * Parameters::GetInstance()->GetEV2AU();
+   this->pm3ParameterK[1] = -0.954920 * Parameters::GetInstance()->GetEV2AU();
+   this->pm3ParameterK[2] = 0.0;
+   this->pm3ParameterK[3] = 0.0;
+   this->pm3ParameterL[0] = 5.976510 / pow(Parameters::GetInstance()->GetAngstrom2AU(),2.0);
+   this->pm3ParameterL[1] = 5.944700 / pow(Parameters::GetInstance()->GetAngstrom2AU(),2.0);
+   this->pm3ParameterL[2] = 0.00;
+   this->pm3ParameterL[3] = 0.00;
+   this->pm3ParameterM[0] = 5.944700 * Parameters::GetInstance()->GetAngstrom2AU();
+   this->pm3ParameterM[1] = 2.328140 * Parameters::GetInstance()->GetAngstrom2AU();
+   this->pm3ParameterM[2] = 0.00;
+   this->pm3ParameterM[3] = 0.00;
+
 }
 }
